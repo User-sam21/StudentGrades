e7c56a9c1c66a9454b91525fa8df9369a5156611
User-sam21 <samirabelmabkhoute@gmail.com>
2024-12-03 01:28:37 +0100

    third commit

--- StudentGrades/Program.cs
+++ StudentGrades/Program.cs
@@ -3,7 +3,7 @@
 //Get pourcentage
 var gradeCalculator = new GradeCalcul();
 
-Console.Write("Entrer svp la pourcentage : ");
+Console.Write("Entrez la pourcentage : ");
 var percentage = Convert.ToInt32(Console.ReadLine());
 
 var grade= gradeCalculator.GetGradeByPercentage(percentage);
